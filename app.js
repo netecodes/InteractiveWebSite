9bab27cf83fe73edfd8cceeeb049b3f719428b46
netecodes <dotneko@gmail.com>
2016-06-14 17:05:16 +0200

    add form and anchor animation

--- app.js
+++ app.js
@@ -10,4 +10,11 @@ $(document).ready(function() {
 		.closest('.news-item')
 		.addClass('is-liked');
 	});
+
+	$('.js-add-link').on('click', function(event) {
+		event.preventDefault();
+
+		$('.js-form').toggleClass('is-visible');
+	
+	});
 });
